0768473da308f9e7ff8f6d0238800d3174fabd56
siro <trimmingdog199x@gmail.com>
2025-08-05 11:34:40 +0900

    p215

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Player = GameObject.Find("player_0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // フレームごとに等速で落下させる
+        transform.Translate(0, -0.1f, 0);
+
+        // 画面外に出たらオブジェクトを破棄する
+        if (transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        // 当たり判定
+        Vector2 p1 = transform.position; // 矢の中心座標
+        Vector2 p2 = this.Player.transform.position; // プレイヤの中心座標
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f; // 矢の半径
+        float r2 = 1.0f; // プレイヤの半径
+
+        if (d < r1 + r2)
+        {
+            // 衝突した場合は矢を消す
+            Destroy(gameObject);
+        }
+    }
+}
